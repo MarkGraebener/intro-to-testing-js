3e89c1e1cef9b3a821a87bac98c8c34a759ae9ab
MarkGraebener <mark.w.graebener@gmail.com>
2021-11-30 17:42:11 -0600

    number 2

--- code.js
+++ code.js
@@ -4,5 +4,10 @@ let helloWorld = (name) => {
 }
 function double(num) {
     return num * 2
-
+}
+let isFive = (num) => {
+    return num === 5;
+}
+let isEven = (num) => {
+    return num % 2 === 0;
 }
--- tests.js
+++ tests.js
@@ -25,3 +25,49 @@ describe("double", function() {
         expect(typeof double).toBe("function")
     });
 })
+
+describe("isFive", () => {
+    it('should exist', function () {
+        expect( typeof isFive).toBe("function")
+    });
+    it('should return a boolean', function () {
+        expect(typeof isFive()).toBe("boolean")
+    });
+    it('should return 5', function () {
+        expect(isFive(5)).toBe(true)
+    });
+    it('should return false', function () {
+        expect(isFive("5")).toBe(false)
+    });
+})
+
+describe("isEven", () => {
+    it('should return a boolean', function () {
+        expect(typeof isEven()).toBe("boolean")
+    });
+    it('2 should return true', function () {
+        expect(isEven(2)).toBe(true)
+    });
+    it('-4 should return true', function () {
+        expect(isEven(-4)).toBe(true)
+    });
+    it('3 should return false', function () {
+        expect(isEven(3)).toBe(false)
+    });
+    it('banana should return false', function () {
+        expect(isEven("banana")).toBe(false)
+    });
+    it('/"8/" should return true', function () {
+        expect(isEven("8")).toBe(true)
+    });
+    it('infinity should return false', function () {
+        expect(isEven(Infinity)).toBe(false)
+    });
+    it('false should return false', function () {
+        expect(isEven(true)).toBe(false)
+    });
+    it('/(/) should return false', function () {
+        expect(isEven()).toBe(false)
+    });
+
+})
